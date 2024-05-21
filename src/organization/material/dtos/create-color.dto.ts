f6f5680b570f582e9e61f4ff8d8fbc2f5344a822
joaosenna19 <101044996+joaosenna19@users.noreply.github.com>
2024-05-21 13:16:20 -0600

    create basic logic for adding materials

---
+++ src/organization/material/dtos/create-color.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateColorDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  pricePerFoot: number;
+}
--- src/organization/material/material.service.ts
+++ src/organization/material/material.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
-export class MaterialService {}
+export class MaterialService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getMaterials() {
+    return await this.prisma.material.findMany();
+  }
+
+  async getMaterial(id: string) {
+    const material = await this.prisma.material.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!material) {
+      throw new NotFoundException('Material not found');
+    }
+
+    return material;
+  }
+}
